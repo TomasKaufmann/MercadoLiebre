1c12ae2eee0b97525596b0bce1a9ebd66bd043d7
tomikaufmann <tomas.kaufmann@osh-pro.com>
2021-07-31 14:47:24 -0300

    cambiado el puerto

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.listen("3030", () => console.log("sv prendido en: http://localhost:3030/home"));
+app.listen(process.env.PORT || 3000 , () => console.log("sv prendido en: http://localhost:3030/home"));
 
 app.get('/home', (req, res) => res.sendFile(path.join(__dirname, "/views/home.html")));
-
 app.get('/register', (req, res) =>  res.sendFile(path.join(__dirname, "/views/register.html")));
 app.get('/login', (req, res) =>  res.sendFile(path.join(__dirname, "/views/login.html")));
 
